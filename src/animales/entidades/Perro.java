e02a007a6cfb6f798e6f9efe3a3fd2eeabb57f8e
emiliano17mza <emiliano17mza@gmail.com>
2021-10-15 18:12:35 -0300

    Se agregan atributos, constructor, getters y setters

--- src/animales/entidades/Perro.java
+++ src/animales/entidades/Perro.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package animales.entidades;
 
-/**
- *
- * @author Emili
- */
 public class Perro {
+    
+    private String nombre;
+    private Integer edad;
+
+    public Perro(String nombre, Integer edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+    
     
 }
