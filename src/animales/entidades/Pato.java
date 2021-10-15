6ae03c2b77d407f36ceef46eeb84b77bc8486f95
EGR777 <julio4902trabajo@gmail.com>
2021-10-15 18:43:11 -0300

    Agrego Rama Pato

--- src/animales/entidades/Pato.java
+++ src/animales/entidades/Pato.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package animales.entidades;
-
-/**
- *
- * @author julio
- */
 public class Pato {
+    
+    private int numPatas;
+    private String nombre;
+
+    public Pato(int numPatas, String nombre) {
+        this.numPatas = numPatas;
+        this.nombre = nombre;
+    }
+
+    public Pato() {
+    }
+
+    public int getNumPatas() {
+        return numPatas;
+    }
+
+    public void setNumPatas(int numPatas) {
+        this.numPatas = numPatas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Pato{" + "numPatas=" + numPatas + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
     
 }
